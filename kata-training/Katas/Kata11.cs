22baa2b93ef270a9b7c545b5ebf28ed1925e7d86
Krista Anderson <107154181+Knderson21@users.noreply.github.com>
2024-09-06 21:15:24 -0400

    Update Kata11.cs
    alt solution

--- kata-training/Katas/Kata11.cs
+++ kata-training/Katas/Kata11.cs
@@ -16,6 +16,8 @@ namespace kata_training.Katas;
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public class Kata11
 {
@@ -33,5 +35,23 @@ public class Kata11
         "rock" => (p2 == "scissors") ? "Player 1 won!" : "Player 2 won!",
         _ => "default"
     };
+  }
+  
+  //alt solution
+  public string Rps2(string p1, string p2)
+  {
+    if (p1 == p2)
+      return "Draw!";
+
+    if (((p1 == "rock") && (p2 == "scissors")) ||
+        ((p1 == "scissors") && (p2 == "paper")) ||
+        ((p1 == "paper") && (p2 == "rock")))
+    {
+      return "Player 1 won!";
+    }
+    else
+    {
+      return "Player 2 won!";
+    }
   }
 }
