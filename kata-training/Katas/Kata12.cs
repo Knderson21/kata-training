09d741295dd0a7f7ef02b90188c784a263405e06
Krista Anderson <107154181+Knderson21@users.noreply.github.com>
2024-09-08 21:39:41 -0400

    Update Kata12.cs

--- kata-training/Katas/Kata12.cs
+++ kata-training/Katas/Kata12.cs
@@ -7,7 +7,7 @@ Your task is to convert strings to how they would be written by Jaden Smith. The
 Example:
 
 Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
-Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+Jaden-Cased:	 "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
 */
 
@@ -16,27 +16,34 @@ namespace kata_training.Katas;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 
 public class Kata12
 {
 	public static string ToJadenCase(this string phrase)
   { 
-        string[] words = phrase.Split(' ');
+		string[] words = phrase.Split(' ');
 
-        for (int i = 0; i < words.Length; i++)
-        {
-            if (words[i].Length > 0)
-            {
-                words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
-            }
-        }
+		for (int i = 0; i < words.Length; i++)
+		{
+			if (words[i].Length > 0)
+			{
+				words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+			}
+		}
 
-        return string.Join(" ", words);
+		return string.Join(" ", words);
   }
   
   //alt solution
   public static string ToJadenCase2(this string phrase)
   {
     return String.Join(" ", phrase.Split().Select(i => Char.ToUpper(i[0]) + i.Substring(1)));
+  }
+  
+  //alt solution2
+  public static string ToJadenCase3(this string phrase)
+  {
+    	return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(phrase);
   }
 }
